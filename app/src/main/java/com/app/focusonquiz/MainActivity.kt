c992d8fa508c7cb83da264d62404a2926f26f09a
Dharmesh Basapati <dharmesh.basapati@spacestem.com>
2021-11-02 12:39:20 +0530

    Added View Binding. Shuffled Correct Answer. Calculated Points.

--- app/src/main/java/com/app/focusonquiz/MainActivity.kt
+++ app/src/main/java/com/app/focusonquiz/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.app.focusonquiz
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.app.focusonquiz.databinding.ActivityMainBinding
 import com.app.focusonquiz.room.builder.DatabaseBuilder
 import com.app.focusonquiz.room.entity.Quiz
 import kotlinx.coroutines.Dispatchers
@@ -14,66 +14,108 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 import java.text.NumberFormat
-import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private var ctimer: CountDownTimer? = null
-    private var btnA: Button? = null
-    private var btnB: Button? = null
-    private var btnC: Button? = null
-    private var btnD: Button? = null
+
     private lateinit var questionsList: List<Quiz>
-    private var tvCounter: TextView? = null
-    private var tvQuestion: TextView? = null
     private var questionNum = 0
+    private var totalScore = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        tvCounter = findViewById<TextView>(R.id.tvCounter)
-        tvQuestion = findViewById<TextView>(R.id.tvQuestion)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnA = findViewById<Button>(R.id.btnA)
-        btnB = findViewById<Button>(R.id.btnB)
-        btnC = findViewById<Button>(R.id.btnC)
-        btnD = findViewById<Button>(R.id.btnD)
+        binding.apply {
 
-        openQuestionsDB()
+            btnStartRestart.setOnClickListener {
+                openQuestionsDB()
+            }
 
-        btnA?.setOnClickListener {
-            ctimer?.cancel()
-            questionNum += 1
-            startCounter()
+            btnA.setOnClickListener {
+                checkAnswerAndCalculatePoints(btnA.text.toString(), false)
+                jumpToNextQuestion()
+            }
+
+            btnB.setOnClickListener {
+                checkAnswerAndCalculatePoints(btnB.text.toString(), false)
+                jumpToNextQuestion()
+            }
+
+            btnC.setOnClickListener {
+                checkAnswerAndCalculatePoints(btnC.text.toString(), false)
+                jumpToNextQuestion()
+            }
+
+            btnD.setOnClickListener {
+                checkAnswerAndCalculatePoints(btnD.text.toString(), false)
+                jumpToNextQuestion()
+            }
+
         }
-        btnB?.setOnClickListener {
-            ctimer?.cancel()
-            questionNum += 1
-            startCounter()
+
+    }
+
+    private fun checkAnswerAndCalculatePoints(answer: String, isSkipped: Boolean) {
+
+        if (isSkipped) {
+            Log.d("###KBC", "IS SKIPPED")
+            totalScore -= 5
+        } else {
+            if (answer == questionsList[questionNum].opt1) {
+                // Add 20
+                Log.d("###KBC", "CORRECT ANSWER")
+                totalScore += 20
+            } else {
+                // Minus 10
+                Log.d("###KBC", "WRONG ANSWER")
+                totalScore -= 10
+            }
         }
-        btnC?.setOnClickListener {
-            ctimer?.cancel()
-            questionNum += 1
-            startCounter()
-        }
-        btnD?.setOnClickListener {
-            ctimer?.cancel()
-            questionNum += 1
-            startCounter()
-        }
+
+        Log.d("###KBC", "Current Total Score = $totalScore ")
+    }
+
+    private fun jumpToNextQuestion() {
+        binding.tvCounter.setTextColor(Color.DKGRAY)
+        binding.tvCounter.text = "10"
+        ctimer?.cancel()
+        questionNum += 1
+        startCounter()
     }
 
     private fun openQuestionsDB() {
 
         GlobalScope.launch(Dispatchers.IO) {
+
             questionsList =
                 DatabaseBuilder.getDBInstance(applicationContext).focusDao().getQuestions()
             Log.d("TAG", "openQuestionsDB: $questionsList")
+
         }.invokeOnCompletion {
+
             GlobalScope.launch(Dispatchers.Main) {
+
+                binding.apply {
+                    btnStartRestart.visibility = View.GONE
+                    tvQuestion.visibility = View.VISIBLE
+                    tvCounter.visibility = View.VISIBLE
+                    btnA.visibility = View.VISIBLE
+                    btnB.visibility = View.VISIBLE
+                    btnC.visibility = View.VISIBLE
+                    btnD.visibility = View.VISIBLE
+                }
+
+                questionNum = 0
+                totalScore = 0
+
                 startCounter()
+
             }
         }
 
@@ -82,14 +124,41 @@ class MainActivity : AppCompatActivity() {
 
     private fun startCounter() {
 
-        if (questionNum < 6) {
+        if (questionNum < questionsList.size) {
 
-            tvQuestion?.text = questionsList[questionNum].question
-            btnA?.text = questionsList[questionNum].opt1
-            btnB?.text = questionsList[questionNum].opt2
-            btnC?.text = questionsList[questionNum].opt3
-            btnD?.text = questionsList[questionNum].opt4
+            binding.apply {
 
+                tvQuestion.text = "${questionNum + 1}. " + questionsList[questionNum].question
+
+                when (arrayListOf("A", "B", "C", "D").random()) {
+
+                    "A" -> {
+                        btnA.text = questionsList[questionNum].opt1
+                        btnB.text = questionsList[questionNum].opt2
+                        btnC.text = questionsList[questionNum].opt3
+                        btnD.text = questionsList[questionNum].opt4
+                    }
+                    "B" -> {
+                        btnB.text = questionsList[questionNum].opt1
+                        btnA.text = questionsList[questionNum].opt2
+                        btnC.text = questionsList[questionNum].opt3
+                        btnD.text = questionsList[questionNum].opt4
+                    }
+                    "C" -> {
+                        btnC.text = questionsList[questionNum].opt1
+                        btnB.text = questionsList[questionNum].opt2
+                        btnA.text = questionsList[questionNum].opt3
+                        btnD.text = questionsList[questionNum].opt4
+                    }
+                    "D" -> {
+                        btnD.text = questionsList[questionNum].opt1
+                        btnB.text = questionsList[questionNum].opt2
+                        btnC.text = questionsList[questionNum].opt3
+                        btnA.text = questionsList[questionNum].opt4
+                    }
+                }
+
+            }
 
             ctimer = object : CountDownTimer(11000, 1000) {
 
@@ -99,26 +168,33 @@ class MainActivity : AppCompatActivity() {
                     val hour = millisUntilFinished / 3600000 % 24
                     val min = millisUntilFinished / 60000 % 60
                     val sec = millisUntilFinished / 1000 % 60
-                    tvCounter?.text = f.format(sec)
+                    if (Integer.parseInt(f.format(sec)) < 4) {
+                        binding.tvCounter.setTextColor(Color.RED)
+                    }
+                    binding.tvCounter.text = f.format(sec)
 
                 }
 
                 override fun onFinish() {
-                    tvCounter?.text = "10"
-                    questionNum += 1
-                    startCounter()
+                    checkAnswerAndCalculatePoints("", true)
+                    jumpToNextQuestion()
                 }
 
             }.start()
 
         } else {
 
-            tvQuestion?.text = "Your score is 5 out of 5"
-            btnA?.visibility = View.GONE
-            btnB?.visibility = View.GONE
-            btnC?.visibility = View.GONE
-            btnD?.visibility = View.GONE
+            binding.apply {
+                tvQuestion.text = "Your total score is $totalScore out of 400"
+                tvCounter.visibility = View.GONE
+                btnA.visibility = View.GONE
+                btnB.visibility = View.GONE
+                btnC.visibility = View.GONE
+                btnD.visibility = View.GONE
+                btnStartRestart.visibility = View.VISIBLE
+                btnStartRestart.text = "RESTART QUIZ"
 
+            }
 
         }
 
